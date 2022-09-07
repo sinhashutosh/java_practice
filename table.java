c41bf52dd271a65bb5268e7bd0d33b169fa84924
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-08 00:05:50 +0530

    Table of 2 to 30 row wise completed

---
+++ table.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class table {
+    public static void main(String[] args) {
+        for(int i=2;i<=30;i++) {
+            for(int j=1;j<=10;j++) {
+                System.out.print(i*j);
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
