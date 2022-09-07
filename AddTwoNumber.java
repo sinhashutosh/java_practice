2e8f34d21dc6aeac839da4df5a2a359c754b1a5d
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-07 18:20:45 +0530

    program of add two number completed

---
+++ AddTwoNumber.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class AddTwoNumber {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 30;
+        int sum;
+        sum = num1 + num2;
+        System.out.println("Sum of Two Number is " + sum);
+    }
+}
