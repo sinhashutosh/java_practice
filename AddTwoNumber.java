c85a319fedd5c996904aef6388824115fce81255
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-07 21:16:25 +0530

    User input added

--- AddTwoNumber.java
+++ AddTwoNumber.java
@@ -1,9 +1,16 @@
 package org.example;
+//import util.java.scanner;
 
+import java.util.Scanner;
+
+//import java.util.*; This import all the method of util class
 public class AddTwoNumber {
     public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 30;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Integer Number ");
+        int num1= sc.nextInt();
+        System.out.println("Enter Second Integer Number ");
+        int num2= sc.nextInt();
         int sum;
         sum = num1 + num2;
         System.out.println("Sum of Two Number is " + sum);
