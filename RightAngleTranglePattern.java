27f4c56e8836cef49d385b39f243ee2b6ba478fb
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-08 06:40:14 +0530

    Second pattern

--- RightAngleTranglePattern.java
+++ RightAngleTranglePattern.java
@@ -4,7 +4,7 @@ public class RightAngleTranglePattern {
     public static void main(String[] args) {
         for(int i=1;i<=10;i++) {
             for(int j=1;j<=i;j++) {
-                System.out.print("*");
+                System.out.print(i);
             }
             System.out.println("");
         }
