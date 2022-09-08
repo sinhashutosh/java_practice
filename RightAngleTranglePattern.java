bfb4168f38eaa513047e96f52a1ff17c5a94bea1
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-08 06:51:20 +0530

    Third pattern

--- RightAngleTranglePattern.java
+++ RightAngleTranglePattern.java
@@ -2,9 +2,12 @@ package org.example;
 
 public class RightAngleTranglePattern {
     public static void main(String[] args) {
-        for(int i=1;i<=10;i++) {
+        int k=1;
+        for(int i=1;i<=4;i++) {
             for(int j=1;j<=i;j++) {
-                System.out.print(i);
+                System.out.print(k);
+                k++;
+                System.out.print(" ");
             }
             System.out.println("");
         }
